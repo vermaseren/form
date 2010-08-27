3ed0a65f8349d642108cdd6e3bce5990cabe5a9a
t68 <t68>
2010-08-27 09:24:30 +0000

    All the copyright commentary added to each file.

--- sources/names.c
+++ sources/names.c
@@ -6,6 +6,32 @@
  *	The file also contains the utility routines for maintaining the
  *	balanced trees that make searching for names rather fast.
  */
+/* #[ License : */
+/*
+ *   Copyright (C) 1984-2010 J.A.M. Vermaseren
+ *   When using this file you are requested to refer to the publication
+ *   J.A.M.Vermaseren "New features of FORM" math-ph/0010025
+ *   This is considered a matter of courtesy as the development was paid
+ *   for by FOM the Dutch physics granting agency and we would like to
+ *   be able to track its scientific use to convince FOM of its value
+ *   for the community.
+ *
+ *   This file is part of FORM.
+ *
+ *   FORM is free software: you can redistribute it and/or modify it under the
+ *   terms of the GNU General Public License as published by the Free Software
+ *   Foundation, either version 3 of the License, or (at your option) any later
+ *   version.
+ *
+ *   FORM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *   FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *   details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with FORM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/* #] License : */ 
 /*
   	#[ Includes :
 */
