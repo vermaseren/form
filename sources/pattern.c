f15decffd6a345405c0bdedecd250e5e479ac687
t68 <t68>
2011-04-06 11:56:02 +0000

    Yet another (similar) bug in FindAll.

--- sources/pattern.c
+++ sources/pattern.c
@@ -1464,7 +1464,7 @@ Hitlevel2:							level2 = level;
 														m[SUBEXPSIZE+8] = SYMTONUM;
 														m[SUBEXPSIZE+11] = jj;
 													}
-													if ( m[SUBEXPSIZE+7] >= 0 ) {
+													else if ( m[SUBEXPSIZE+7] >= 0 ) {
 														oldvv = *(bfirst - Sets[j].first
 														+ Sets[m[SUBEXPSIZE+7]].first);
 													}
