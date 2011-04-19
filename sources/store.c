530effa696aa5ef04b42be5849ad317a90205600
t68 <t68>
2011-04-19 12:18:20 +0000

    Added debug tools

--- sources/store.c
+++ sources/store.c
@@ -2881,7 +2881,7 @@ GetTb3:
 	}
 	}
 /*
-			#] Indices :
+			#] Indices : 
 			#[ Vectors :
 */
 	{
@@ -3051,7 +3051,7 @@ ErrGt2:
 }
 
 /*
- 		#] GetTable :
+ 		#] GetTable : 
  		#[ CopyExpression :
 
 		Copies from one scratch buffer to another.
@@ -3226,6 +3226,46 @@ WriteTrailer:
 
 /*
  		#] CopyExpression : 
+ 		#[ ExprStatus :
+*/
+
+#ifdef HIDEDEBUG
+
+static UBYTE *statusexpr[] = {
+	 (UBYTE *)"LOCALEXPRESSION"
+	,(UBYTE *)"SKIPLEXPRESSION"
+	,(UBYTE *)"DROPLEXPRESSION"
+	,(UBYTE *)"DROPPEDEXPRESSION"
+	,(UBYTE *)"GLOBALEXPRESSION"
+	,(UBYTE *)"SKIPGEXPRESSION"
+	,(UBYTE *)"DROPGEXPRESSION"
+	,(UBYTE *)"UNKNOWN"
+	,(UBYTE *)"STOREDEXPRESSION"
+	,(UBYTE *)"HIDDENLEXPRESSION"
+	,(UBYTE *)"HIDELEXPRESSION"
+	,(UBYTE *)"DROPHLEXPRESSION"
+	,(UBYTE *)"UNHIDELEXPRESSION"
+	,(UBYTE *)"HIDDENGEXPRESSION"
+	,(UBYTE *)"HIDEGEXPRESSION"
+	,(UBYTE *)"DROPHGEXPRESSION"
+	,(UBYTE *)"UNHIDEGEXPRESSION"
+	,(UBYTE *)"INTOHIDELEXPRESSION"
+	,(UBYTE *)"INTOHIDEGEXPRESSION"
+};
+
+void ExprStatus(EXPRESSIONS e)
+{
+	MesPrint("Expression %s(%d) has status %s(%d,%d). Buffer: %d, Position: %15p",
+		AC.exprnames->namebuffer+e->name,(WORD)(e-Expressions),
+		statusexpr[e->status],e->status,e->hidelevel,
+		e->whichbuffer,&(e->onfile));
+}
+
+#endif
+
+/*
+ 		#] ExprStatus :
+
 	#] StoreExpressions :
 	#[ System Independent Saved Expressions :
 
