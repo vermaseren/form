4de9967a29bf02f629aa2cc66e2e336ee7c5e163
Takahiro Ueda <tueda@nikhef.nl>
2017-06-29 11:12:44 +0200

    [test] Add a benchmark test measuring the form unit per hour

--- check/check-help.sh
+++ check/check-help.sh
@@ -1,8 +1,8 @@
-#! /bin/sh
+#!/bin/sh
 # This is intended to be run from "make check".
 trap 'exit 1' 1 2 13 15
 status=0
 for form in $TEST_BINS; do
-  "$RUBY" "$CHECK_RB" "$form" $TEST_OPTS || status=1
+  "$RUBY" "$srcdir/check.rb" "$form" $TEST_OPTS || status=1
 done
 exit $status
