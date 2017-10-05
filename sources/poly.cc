e449d0c4ca20043c86089eb447c1eab20ef00189
Takahiro Ueda <tueda@nikhef.nl>
2017-10-05 10:56:47 +0200

    Add move semantics to the poly class

--- sources/poly.cc
+++ sources/poly.cc
@@ -110,6 +110,9 @@ poly::poly (PHEAD const UWORD *a, WORD na, WORD modp, WORD modn):
 poly::poly (const poly &a, WORD modp, WORD modn):
 	size_of_terms(AM.MaxTer/(LONG)sizeof(WORD))	
 {
+#ifdef POLY_MOVE_DEBUG
+	std::cout << "poly copy ctor" << std::endl;
+#endif
 	POLY_GETIDENTITY(a);
 	POLY_STOREIDENTITY;
 	
@@ -1933,7 +1936,9 @@ void poly::mod (const poly &a, const poly &b, poly &c) {
 
 // copy operator
 poly & poly::operator= (const poly &a) {
-
+#ifdef POLY_MOVE_DEBUG
+	std::cout << "poly copy assign" << std::endl;
+#endif
 	if (&a != this) {
 		modp = a.modp;
 		modn = a.modn;
