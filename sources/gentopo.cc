cb1b29f8540e3887148f201e4f4d9f8e83b6e12e
vermaseren <t68@nikhef.nl>
2018-11-02 13:50:36 +0900

    Added allignment in gentopo.cc to avoid compiler warning

--- sources/gentopo.cc
+++ sources/gentopo.cc
@@ -254,6 +254,7 @@ class MNodeClass {
     int **clmat;                   // matrix used for classification
     int  *flist;                   // the first node in each class
     int   maxdeg;                  // maximal value of degree(node)
+	int   forallignment;
 
     MNodeClass(int nnodes, int ncl);
     ~MNodeClass();
