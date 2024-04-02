vermaseren/form

commit 34c76e35117a4794e819a63d800239aa9be66230

build: use alarm() only on UNIX
The alarm() function is included in POSIX but not in the C standard
library. It may not be available on some platforms like Windows.

sources/pre.c
@@ -7162,9 +7162,14 @@ int DoPrePrependPath(UBYTE *s)
 
 int DoTimeOutAfter(UBYTE *s)
 {
+#ifdef HAVE_ALARM
 	ULONG x;
+#else
+	DUMMYUSE(s);
+#endif
 	if ( AP.PreSwitchModes[AP.PreSwitchLevel] != EXECUTINGPRESWITCH ) return(0);
 	if ( AP.PreIfStack[AP.PreIfLevel] != EXECUTINGIF ) return(0);
+#ifdef HAVE_ALARM
 	while ( *s == ' ' || *s == '\t' ) s++;
 	x = 0;
 	while ( FG.cTable[*s] == 1 ) {
@@ -7179,6 +7184,10 @@ int DoTimeOutAfter(UBYTE *s)
 		MesPrint("@proper syntax is #TimeoutAfter number");
 		return(-1);
 	}
+#else
+	Error0("#timeoutafter not implemented on this computer/system");
+	return(-1);
+#endif
 }
 
 /*

sources/startup.c
@@ -1681,7 +1681,9 @@ int main(int argc, char **argv)
 	if ( !AM.FromStdin ) PrintHeader(1);
 	IniVars();
 	Globalize(1);
+#ifdef HAVE_ALARM
 	if ( AM.TimeLimit > 0 ) alarm(AM.TimeLimit);
+#endif
 	TimeCPU(0);
 	TimeChildren(0);
 	TimeWallClock(0);

sources/unix.h
@@ -55,3 +55,4 @@ the errno checkup*/
 #define ALTSEPARATOR '/'
 #define PATHSEPARATOR ':'
 #define WITH_ENV
+#define WITH_ALARM

commit d587b0321397a9e3c16bb1c06516c62f9d078f49

fix: conditional compilation logic bug introduced in 34c76e3
- Correct HAVE_ALARM -> WITH_ALARM.
- Add test cases for #TimeoutAfter.

check/features.frm
@@ -248,6 +248,36 @@ assert result("F") =~ expr("1234")
 assert result("G") =~ expr("5678")
 assert result("H") =~ expr("9012")
 *--#] AppendPath :
+*--#[ TimeoutAfter_1 :
+#procedure problematicprocedure
+* Do nothing.
+#endprocedure
+
+#timeoutafter 1000
+#call problematicprocedure
+#timeoutafter 0
+.end
+#require unix?
+assert succeeded?
+*--#] TimeoutAfter_1 :
+*--#[ TimeoutAfter_2 :
+#procedure problematicprocedure
+* Infinite loop.
+  #do i=1,1
+    #redefine i "0"
+  #enddo
+#endprocedure
+
+#timeoutafter 1
+#call problematicprocedure
+#timeoutafter 0
+.end
+#require unix?
+# ParFORM may terminate without printing the error message,
+# depending on the MPI environment.
+#pend_if mpi?
+assert runtime_error?
+*--#] TimeoutAfter_2 :
 *--#[ dedup :
 * Test deduplication
 #-

sources/pre.c
@@ -7162,14 +7162,14 @@ int DoPrePrependPath(UBYTE *s)
 
 int DoTimeOutAfter(UBYTE *s)
 {
-#ifdef HAVE_ALARM
+#ifdef WITH_ALARM
 	ULONG x;
 #else
 	DUMMYUSE(s);
 #endif
 	if ( AP.PreSwitchModes[AP.PreSwitchLevel] != EXECUTINGPRESWITCH ) return(0);
 	if ( AP.PreIfStack[AP.PreIfLevel] != EXECUTINGIF ) return(0);
-#ifdef HAVE_ALARM
+#ifdef WITH_ALARM
 	while ( *s == ' ' || *s == '\t' ) s++;
 	x = 0;
 	while ( FG.cTable[*s] == 1 ) {

sources/startup.c
@@ -1681,7 +1681,7 @@ int main(int argc, char **argv)
 	if ( !AM.FromStdin ) PrintHeader(1);
 	IniVars();
 	Globalize(1);
-#ifdef HAVE_ALARM
+#ifdef WITH_ALARM
 	if ( AM.TimeLimit > 0 ) alarm(AM.TimeLimit);
 #endif
 	TimeCPU(0);