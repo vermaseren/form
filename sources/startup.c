673a7f0483e0e4cc3510aed46ac93451b1746e92
Takahiro Ueda <t.ueda.od@juntendo.ac.jp>
2024-04-29 19:00:03 +0900

    fix: use binary mode for stdout on Windows
    On Windows, the standard output is opened with text mode. This patch
    sets the mode to binary mode, which partially resolves #418.

--- sources/startup.c
+++ sources/startup.c
@@ -1613,6 +1613,9 @@ int main(int argc, char **argv)
 #ifdef TRAPSIGNALS
 	setSignalHandlers();
 #endif
+#ifdef WINDOWS
+	_setmode(_fileno(stdout),O_BINARY);
+#endif
 
 #ifdef WITHPTHREADS
 	AB = ABdummy;
