62f0ea2ca2c3cbdb42e5d7e6be771c7146915f41
Josh Davies <jodavies@users.noreply.github.com>
2024-12-04 15:46:37 +0000

    compiler warning: unused-parameter
    Use FORM's DUMMYUSE macro to suppress (valid) warning for an unused parameter.

--- sources/setfile.c
+++ sources/setfile.c
@@ -845,6 +845,7 @@ VOID WriteSetup()
 SORTING *AllocSort(LONG inLargeSize, LONG inSmallSize, LONG inSmallEsize, LONG inTermsInSmall,
                    int inMaxPatches, int inMaxFpatches, LONG inIOsize, int level)
 {
+	DUMMYUSE(level); /* This is only used in DEBUGGING mode */
 	LONG LargeSize = inLargeSize;
 	LONG SmallSize = inSmallSize;
 	LONG SmallEsize = inSmallEsize;
